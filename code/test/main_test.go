440071f9800de2d976355ba39fc007e2f148e7f3
arwa123456 <58654323+arwa123456@users.noreply.github.com>
2022-08-23 12:20:25 +0200

    Update main_test.go

--- code/test/main_test.go
+++ code/test/main_test.go
@@ -2,59 +2,25 @@ package test
 
 import (
 	"testing"
-        "os"
-	"path"
-	"github.com/gruntwork-io/terratest/modules/terraform"
-        terraformCore "github.com/hashicorp/terraform/terraform"
 
-	//"github.com/stretchr/testify/assert"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+
+//	"github.com/stretchr/testify/assert"
 )
 
-func TestUT_StorageAccountName(t *testing.T) {
-	t.Parallel()
+func TestTerraformHelloWorldExample(t *testing.T) {
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../example",
+	})
 
-	// Test cases for storage account name conversion logic
-	testCases := map[string]string{
-		"TestWebsiteName": "testwebsitenamedata001",
-		"ALLCAPS":         "allcapsdata001",
-		"S_p-e(c)i.a_l":   "specialdata001",
-		"A1phaNum321":     "a1phanum321data001",
-		"E5e-y7h_ng":      "e5ey7hngdata001",
-	}
+	//defer terraform.Destroy(t, terraformOptions)
 
-	for input, expected := range testCases {
-		// Specify the test case folder and "-var" options
-		tfOptions := &terraform.Options{
-			TerraformDir: "./fixtures/storage-account-name",
-			Vars: map[string]interface{}{
-				"website_name": input,
-			},
-		}
+	terraform.Init(t, terraformOptions)
+  defer terraform.Destroy(t, terraformOptions)
+  // When the test is completed, teardown the infrastructure by calling terraform destroy
+ 
 
-		// Terraform init and plan only
-		tfPlanOutput := "terraform.tfplan"
-		terraform.Init(t, tfOptions)
-		terraform.RunTerraformCommand(t, tfOptions, terraform.FormatArgs(tfOptions, "plan", "-out="+tfPlanOutput)...)
-
-		// Read and parse the plan output
-		f, err := os.Open(path.Join(tfOptions.TerraformDir, tfPlanOutput))
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer f.Close()
-		plan, err := terraformCore.ReadPlan(f)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		// Validate the test result
-		for _, mod := range plan.Diff.Modules {
-			if len(mod.Path) == 2 && mod.Path[0] == "root" && mod.Path[1] == "staticwebpage" {
-				actual := mod.Resources["azurerm_storage_account.main"].Attributes["name"].New
-				if actual != expected {
-					t.Fatalf("Expect %v, but found %v", expected, actual)
-				}
-			}
-		}
-	}
+	//output := terraform.Output(t, terraformOptions, "hello_world")
+	//assert.Equal(t, "Hello, World!", output)
 }
