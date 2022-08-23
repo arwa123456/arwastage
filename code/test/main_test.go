a791509135fcd3606eb5ae396f9c2ce887b910a0
arwa123456 <58654323+arwa123456@users.noreply.github.com>
2022-08-23 12:22:48 +0200

    Update main_test.go

--- code/test/main_test.go
+++ code/test/main_test.go
@@ -6,4 +6,22 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 
 //	"github.com/stretchr/testify/assert"
+	)
+
+func TestTerraformHelloWorldExample(t *testing.T) {
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../example",
+	})
+
+	//defer terraform.Destroy(t, terraformOptions)
+
+	terraform.Init(t, terraformOptions)
+  defer terraform.Destroy(t, terraformOptions)
+  // When the test is completed, teardown the infrastructure by calling terraform destroy
+ 
+
+	//output := terraform.Output(t, terraformOptions, "hello_world")
+	//assert.Equal(t, "Hello, World!", output)
+}
 
