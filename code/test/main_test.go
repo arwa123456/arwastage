547ddc9d5d35e0170027a6234e6c810ca244a50a
arwa123456 <58654323+arwa123456@users.noreply.github.com>
2022-08-23 11:50:49 +0200

    Update main_test.go

--- code/test/main_test.go
+++ code/test/main_test.go
@@ -7,16 +7,58 @@ import (
 	//"github.com/stretchr/testify/assert"
 )
 
-func TestTerraformHelloWorldExample(t *testing.T) {
-	// retryable errors in terraform testing.
-	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../example",
-	})
+type plan struct {
+	Version string `json:"format_version"`
+	Planned struct {
+		Outputs struct {
+			StorageName struct {
+				Value string `json:"value"`
+			} `json:"website_storage_name"`
+		} `json:"outputs"`
+	} `json:"planned_values"`
+}
 
-	//defer terraform.Destroy(t, terraformOptions)
+func TestUT_StorageAccountName_Plan(t *testing.T) {
+	t.Parallel()
 
-	terraform.Init(t, terraformOptions)
+	testCases := map[string]string{
+		"TestWebsiteName": "testwebsitenamedata001",
+		"ALLCAPS":         "allcapsdata001",
+		"S_p-e(c)i.a_l":   "specialdata001",
+		"A1phaNum321":     "a1phanum321data001",
+		"E5e-y7h_ng":      "e5ey7hngdata001",
+	}
 
-	//output := terraform.Output(t, terraformOptions, "hello_world")
-	//assert.Equal(t, "Hello, World!", output)
+	for input, expected := range testCases {
+		tfOptions := &terraform.Options{
+			TerraformDir: "./fixtures/storage-account-name",
+			Vars: map[string]interface{}{
+				"website_name": input,
+			},
+		}
+
+		tfPlanOutput := "terraform.tfplan"
+
+		terraform.Init(t, tfOptions)
+		terraform.RunTerraformCommand(t, tfOptions, terraform.FormatArgs(tfOptions, "plan", "-out="+tfPlanOutput)...)
+
+		tfOptionsEmpty := &terraform.Options{}
+		planJSON, err := terraform.RunTerraformCommandAndGetStdoutE(
+			t, tfOptions, terraform.FormatArgs(tfOptionsEmpty, "show", "-json", tfPlanOutput)...,
+		)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		res := plan{}
+		json.Unmarshal([]byte(planJSON), &res)
+
+		actualStorageName := res.Planned.Outputs.StorageName.Value
+		if actualStorageName != expected {
+			t.Fatalf("Expected %v, but found %v", expected, actualStorageName)
+		} else {
+			t.Logf("Success: Input:%v, StorageName:%v", input, actualStorageName)
+		}
+	}
 }
